99fbfc72f78bb073c553bfd071e89ad95c302804
adrielarias <67917089+adrielarias@users.noreply.github.com>
2020-12-22 00:58:12 -0500

    Delete pawn.cpp

--- pawn.cpp
+++
@@ -1,8 +0,0 @@
-#include "pawn.h"
-
-Pawn::Pawn(QWidget *parent) : Piece(parent)
-{
-    PieceIcon.load("/home/teren/Downloads/AjedrezTEO-main/korone_listener.png");
-    this->resize(64,64);
-    this->setPixmap(PieceIcon);
-}
